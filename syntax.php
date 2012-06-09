5424174f94ef9045ebb404303204ffc3ad1cfd35
Zaher Dirkey <zaherdirkey@yahoo.com>
2012-06-09 18:18:24 +0300

    initial import

---
+++ syntax.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Info Plugin: Displays information about various DokuWiki internals without "start" page
+ * Based on: nslist plugin by Andreas Gohr <andi@splitbrain.org>
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Zaher Dirkey <zaherdirkey@yahoo.com>
+ * @similar to http://www.dokuwiki.org/plugin:clearfloat
+ * @desc use \\\ at end of line to add clearer tag with break live
+ */
+
+/*todo
+  add carrier return for windows file \n?\r
+*/
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+class syntax_plugin_clearer extends DokuWiki_Syntax_Plugin {
+
+  function getInfo(){
+        return array(
+            'author' => 'Zaher Dirkey',
+            'email'  => 'zaherdirkey@yahoo.com',
+            'date'   => '2012-06-4',
+            'name'   => 'Add clearer div class',
+            'desc'   => 'Add clearer div class using \\ chars before new line char.',
+            'url'    => 'http://dokuwiki.org/plugin:clearer',
+        );
+    }
+    function getType(){
+        return 'substition';
+    }
+    function getPType(){
+        return 'block';
+    }
+    function getSort(){
+        return 138;
+    }
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\x5C{3}\n', $mode, 'plugin_clearer');
+    }
+    function handle($match, $state, $pos, &$handler){
+        return array($match, $state, $pos);
+    }
+    function render($mode, &$renderer, $data) {
+			if ($mode == 'xhtml') {
+         $renderer->doc .= '<div class="clearer" ></div><br />';
+        return true;
+      }
+      return false;
+    }
+}
